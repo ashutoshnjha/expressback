429722636bf8a5980969a5b13ce3442a58119831
Ashutosh <ashutoshnjha@gmail.com>
2023-10-30 10:27:30 +0530

    Add nodmon and data var

--- routes/pocApi.js
+++ routes/pocApi.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var pocRouter = express.Router();
 
+var data = [
+    {
+        "title": "Career Conselling",
+        "description": "Career Conselling",
+        "completed": false
+    },
+    {
+        "title": "Carrer Guidence",
+        "description": "Carrer Guidence",
+        "completed": false
+    },
+    {
+        "title": "Learning material",
+        "description": "Learning material & resources",
+        "completed": false
+    },
+    {
+        "title": "Up-skill",
+        "description": "Up-skill planning",
+        "completed": false
+    }
+]
 pocRouter.get("/", function(req, res, next) {
-    res.send("My POC API response");
+    res.send(data);
 });
 
 module.exports = pocRouter;
